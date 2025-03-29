77e7b362ea8da050d2d40c21a50995512bec71fc
Minh <mhnguyen@tamu.edu>
2025-03-29 16:21:57 -0500

    fixed app issue 2

--- server.js
+++ server.js
@@ -8,10 +8,13 @@ const app = express();
 
 const corsOptions = {
   origin: 'https://name-is-minh.github.io',
-  methods: ['GET', 'POST'],
+  methods: ['GET', 'POST', 'OPTIONS'],
   allowedHeaders: ['Content-Type'],
+  credentials: false
+
 };
 app.use(cors(corsOptions));
+app.options('*', cors(corsOptions));
 
 const notion = new Client({
     auth: process.env.NOTION_API_KEY,
